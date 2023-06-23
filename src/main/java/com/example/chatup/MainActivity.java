5a169dd65424de8b8a20058d430af567ffcd3b4c
muktachanda <muktachanda03@gmail.com>
2023-06-24 01:46:55 +0530

    Secured API Key

--- src/main/java/com/example/chatup/MainActivity.java
+++ src/main/java/com/example/chatup/MainActivity.java
@@ -97,7 +97,7 @@ public class MainActivity extends AppCompatActivity {
         RequestBody body = RequestBody.create(jsonBody.toString(),JSON);
         Request request = new Request.Builder()
                 .url("https://api.openai.com/v1/completions")
-                .header("Authorization","Bearer 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")
+                .header("Authorization","Bearer " + getString(R.string.api_key))
                 .post(body)
                 .build();
 
